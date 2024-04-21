5a1350fc7aeb9bec3f8d83d6d8b9391df8c4f9f0
Алексей Бельченков <u608110@gmail.com>
2024-04-21 14:10:25 +0400

    Add Clerk Auth

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { authMiddleware } from '@clerk/nextjs/server';
+
+export default authMiddleware({
+    publicRoutes: ["/"]
+});
+
+export const config = {
+    matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
